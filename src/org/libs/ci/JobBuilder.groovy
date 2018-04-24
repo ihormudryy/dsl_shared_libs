950364a3c8d3fb81041ef79260ae2394a9d3a222
Ihor Mudryy <ihor.mudryy@here.com>
2018-04-24 15:08:22 +0200

    Added JobBuilder class

--- src/org/libs/ci/JobBuilder.groovy
+++ src/org/libs/ci/JobBuilder.groovy
@@ -1,11 +1,13 @@
 package org.libs.ci
 
 import hudson.plugins.git.GitSCM
+import hudson.plugins.git.BranchSpec
 import com.cloudbees.jenkins.GitHubPushTrigger
 import com.coravy.hudson.plugins.github.GithubProjectProperty
 import com.google.common.collect.Collections2
 import com.google.common.collect.Lists
 import hudson.plugins.git.extensions.GitSCMExtension
+import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
 
 def createSimplePipelineJob(repoName, jobName, credId, jenkinsfile) {
     def jenkins = Jenkins.instance
@@ -13,15 +15,14 @@ def createSimplePipelineJob(repoName, jobName, credId, jenkinsfile) {
     def projectProperty = new GithubProjectProperty(repoName)
     def repo_list = GitSCM.createRepoList(repoName, credId)
     def scm =  new GitSCM(repo_list,
-                          [new BranchSpec("**")],
+                          [new BranchSpec("*/master")],
                           false,
                           Collections.<SubmoduleConfig>emptyList(),
                           null, null,
                           Collections.<GitSCMExtension>emptyList()
                           )
     def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(jenkins, jobName)
-    job.setDefinition(new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm,
-                                                                                  jenkinsfile))
+    job.setDefinition(new CpsScmFlowDefinition(scm, jenkinsfile))
     job.setTriggers([gitTrigger])
     job.addProperty(projectProperty)
     jenkins.reload()
